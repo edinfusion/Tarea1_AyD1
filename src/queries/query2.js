8d2717fbc4a1e64864d3627c2b7a2cd4d3eddde3
EdinMV <edinfusion@gmail.com>
2023-02-15 17:48:39 -0600

    Se agrego endpoint get
    Este endpoint devuelve un mensaje de confirmacion a traves del metodo get

--- src/queries/query2.js
+++ src/queries/query2.js
@@ -1,1 +1,9 @@
 //aqui van las consultas
+exports.query2 = async (req, res) => {
+    try {
+        res.status(200).send("<h1>Hola primera tarea practica de AyD 1, Carnet - 201709311</h1>");
+
+    } catch (err) {
+        console.error(err.message);
+    }
+}
